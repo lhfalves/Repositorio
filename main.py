5347d234d3096236ebd7970405133d57aa45419f
lhfalves <lhfalves@gmail.com>
2024-03-20 19:54:38 -0300

    Testando a autenticação do arquivo com o GitHub

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 def main():
     print("Hello, World!")
     print("Testando  alterações no arq via Guit")
+    print( 5 // 2)
+
+    curso = "Python"
+    print(f"Bem vindo ao curso de {curso.upper()}!")
+
+    frutas = ["laranja", "maca", "uva"]
+    print(frutas)
+
+    frutas = []
+    print(frutas)
+
+    letras = list("python")
+    print(letras)
+
 
 if __name__ == '__main__':
     main()
